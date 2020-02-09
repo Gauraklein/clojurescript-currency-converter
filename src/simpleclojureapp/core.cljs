e7c6f736f6fa9087376dba0f1b8f686c594d32f5
gauraklein <gaurakklein@gmail.com>
2020-02-09 00:17:23 -0600

    added in a basic structure for conversion app

--- src/simpleclojureapp/core.cljs
+++ src/simpleclojureapp/core.cljs
@@ -6,7 +6,7 @@
 (println "This text is printed from src/simpleclojureapp/core.cljs. Go ahead and edit it and see reloading in action.")
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!" :number 4349}))
+(defonce app-state (atom {:text "CONVERT YOUR CURRENCIES" :number 4349}))
 
 
 ;; routes
@@ -24,19 +24,27 @@
     
 (defn test-fn []
      [:div#test {:style {:color "green"}}
-      [:h1 "TESTING"]
+      [:input]
+      [:button "hello"]
       [:h3 (:text @app-state)]
       [:p [* (:number @app-state) (:number @app-state)]]])
 
 
-(defn hello-world []
+(defn currency-convert []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it chanasdfksadjfslkdge!"]
-   [:p "BROCCOLI"]
-   (* 4 4 4 4)
-   [:div
-    [test-fn]]])
+    [:h1 (:text @app-state)]
+    [:input]
+    [:select
+      [:option "option 1"]
+      [:option "option 2"]]
+    [:h3 "TO"]
+    [:input]
+    [:select
+      [:option "Ooltion 1"]
+      [:option "Option 2"]]
+    [:button "Convert"]])
+
+  
     
 
 
@@ -44,7 +52,7 @@
    
    
 
-(reagent/render-component [hello-world]
+(reagent/render-component [currency-convert]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload [])
